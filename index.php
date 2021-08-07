a566bd2f148b0dba9fd63c09f3f6e7721fa6e0cb
Milutin Gavrilovic <milutingavrilovic@yahoo.com>
2021-08-07 19:03:35 +0200

    Update index.php

--- index.php
+++ index.php
@@ -14,8 +14,8 @@ $route = isset($_GET['route']) ? $_GET['route'] : '';
 #-------------------dozvoljene_stranice------------------#
 $pages = array (
 'default' => './lib/default.php',
-'api' => './lib/api.php',
-'wapi' => './lib/wapi.php'
+'api' => './lib/second.php',
+'wapi' => './lib/third.php'
 );
 
 #------proverava da li je string u nizu veci od nule i da li je u nizu------#
