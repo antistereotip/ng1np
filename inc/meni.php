98e5a14935439f4785b08e64f4188de5e1843240
Milutin Gavrilovic <milutingavrilovic@yahoo.com>
2021-08-07 19:06:30 +0200

    Update meni.php

--- inc/meni.php
+++ inc/meni.php
@@ -1,12 +1,7 @@
 <nav>
 <li><a href="?route=default">[ - Default - ]</a></li>
-<li><a href="?route=api">[ - Api - ]</a></li>
-<li><a href="?route=wapi">[ - Wapi - ]</a></li>
+<li><a href="?route=second">[ - Second - ]</a></li>
+<li><a href="?route=third">[ - Third - ]</a></li>
 </nav>
 <p></p>
-<label for="disk_c">C:</label>
-<meter id="disk_c" value="3" min="0" max="10"></meter><br>
-
-<label for="disk_d">D:</label>
-<meter id="disk_d" value="0.6"></meter>
 <hr />
