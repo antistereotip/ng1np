d8af4d35da75c0efefb0d3df9b5c708ef643a668
Milutin Gavrilovic <milutingavrilovic@yahoo.com>
2021-08-14 07:27:22 +0200

    Update footer.php

--- inc/footer.php
+++ inc/footer.php
@@ -1,5 +1,6 @@
 <footer>
-<p style="color: #fff;">Ng1np @ 2013 - 2021 @ hightech</p>
+<p style="color: #fff;">Ng1np &#9827; 2013 - 2021 &#9827; hightech</p>
+  <p style="color: yellow;">Powered By Antistereotip</p>
 </footer>
 </body>
 </html>
